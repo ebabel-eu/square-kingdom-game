7e829413133808465d9a1d69b91f40d196b60caa
Nadjib Amar <hello@ebabel.eu>
2016-12-17 02:54:27 +0100

    fix for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import './index.scss';
-
 import App from './components/app/app';
 
 render(<App />, document.getElementById('skg-container'));
